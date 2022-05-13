17df6cd3b5f55065a5931e55a4de65eda500cf83
Yuri Serafim <yuridefranca@gmail.com>
2022-05-12 22:48:55 -0300

    Implemented Service and Repository layers

---
+++ www/app/Providers/RepositoryProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\BaseRepository;
+use App\Repositories\Interface\BaseRepositoryInterface;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(BaseRepositoryInterface::class, BaseRepository::class);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
