26410f707a5c7b60840c6340ecc7395be60f5a3f
Yuri Serafim <yuridefranca@gmail.com>
2024-01-08 18:49:05 -0300

    chore: melhorar scripts para iniciar e parar o docker

--- stopDocker.sh
+++ stopDocker.sh
@@ -1,3 +1,28 @@
 #!/usr/bin/env bash
 
+export $(grep -v '^#' .env | xargs)
+
+while getopts r: argument; do
+	case "${argument}" in
+	r) remove=${OPTARG} ;;
+	esac
+done
+
+function removeNodeModules() {
+	sudo rm -rf www/node_modules
+}
+
+function removeDockerImage() {
+	docker rmi "$PROJECT_NAME/php*"
+}
+
 docker-compose down -v
+
+if [ "$remove" == "node_modules" ]; then
+	removeNodeModules
+elif [ "$remove" == "image" ]; then
+	removeDockerImage
+elif [ "$remove" == "all" ]; then
+	removeDockerImage
+	removeNodeModules
+fi
